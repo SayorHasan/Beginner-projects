c777ee310b956ce38622cc27fb215832b203b244
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2026-04-12 00:10:05 +0600

    done

--- People Counter App/index.js
+++ People Counter App/index.js
@@ -1,26 +1,34 @@
 
-
-
-let count = 0 ;
-let countEl = document.getElementById("count-el") ;
+let count = 0 
+let totalCOUNT = 0
+let countEl = document.getElementById("count-el") 
 
 function increment() {
 
-    count = count + 1;
-    countEl.innerText = count;
+    count = count + 1
+    countEl.innerText = count
 }
 
 function save() {
     
-    countEl.innerText = `People entered: ${countEl.innerText}`
-    previous_count();
+    
+    previous_count()
     
 }
 
 let previousCOUNT = document.getElementById("previous-count");
-previousCOUNT.innerText = "Previous count "
+
 
 function previous_count() {
-    previousCOUNT.innerText +=  " - "+ `${count}`
+    totalCOUNT += count 
+    previousCOUNT.innerText += " "+ `${count}` + " - "
+    countEl.innerText = 0 
+    count = 0
     
+}
+
+function totalC() {
+    previousCOUNT.innerText =  `Total People's Count of today : ${totalCOUNT}`
+    totalCOUNT = 0
+
 }
