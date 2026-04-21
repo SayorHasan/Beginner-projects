db36524eaa96b63688f32fb8bb39c57df5cc8b9b
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2026-04-21 14:13:05 +0600

    blackjackgame

---
+++ BlackJack Game/index.js
@@ -0,0 +1,14 @@
+let firstCard = 10
+let secondCard = 12
+
+let sum = firstCard + secondCard
+
+if (sum < 21){
+    console.log("Do you ant to draw a new card?")
+
+} else if (sum == 21){
+    console.log("Wohoo! You've got Blackjack! ")
+
+} else {
+    console.log("You're out of the game! ")
+}
