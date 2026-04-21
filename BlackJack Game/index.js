05e9869ab184b73e3511bb53a627e0b858f8b7ce
SayorHasan <md.sayor.hasan@g.bracu.ac.bd>
2026-04-21 15:38:33 +0600

    used for loop to render recent cards

--- BlackJack Game/index.js
+++ BlackJack Game/index.js
@@ -1,14 +1,44 @@
 let firstCard = 10
-let secondCard = 12
+let secondCard = 8
+let newcardN = 3
 
 let sum = firstCard + secondCard
+let message = ""
+let cards = [firstCard,secondCard]
 
-if (sum < 21){
-    console.log("Do you ant to draw a new card?")
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("card-el")
 
-} else if (sum == 21){
-    console.log("Wohoo! You've got Blackjack! ")
 
-} else {
-    console.log("You're out of the game! ")
+function startGame(){
+    renderGame()
+}
+function renderGame(){
+    cardEl.textContent = "Cards:"
+    for (let count = 0; count<cards.length; count+=1){
+        cardEl.textContent += " "+ cards[count]
+    }
+    
+    sumEl.textContent = `Sum: ${sum}`
+
+    if (sum < 21){
+        message = "Do you ant to draw a new card?"
+
+    } else if (sum == 21){
+        message= "Wohoo! You've got Blackjack! "
+
+    } else {
+        message = "You're out of the game! "
+    }
+    messageEl.textContent  = message
+    
+}
+
+
+function newCard(){
+    
+    cards.push(newcardN)
+    sum += newcardN
+    renderGame()
 }
